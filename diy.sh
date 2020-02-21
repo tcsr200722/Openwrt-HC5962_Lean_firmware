32bd62898e3da4701a2c6c6e04760719e402aa49
ch1993128 <48594475+ch1993128@users.noreply.github.com>
2020-02-21 17:33:13 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,13 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+git clone https://github.com/rosywrt/luci-theme-rosy
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial
+git clone https://github.com/openwrt-dev/po2lmo.git
+pushd po2lmo
+make && sudo make install
+popd
+./scripts/feeds update -a
+./scripts/feeds install -a
