94fa904848fc957b323a984b54bfb31747c24ca8
ch1993128 <48594475+ch1993128@users.noreply.github.com>
2020-03-01 08:58:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,8 +9,8 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
-git clone https://github.com/rosywrt/luci-theme-rosy
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial
+git clone https://github.com/rosywrt/luci-theme-rosy package/luci-theme-rosy
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
 git clone https://github.com/openwrt-dev/po2lmo.git
 pushd po2lmo
 make && sudo make install
