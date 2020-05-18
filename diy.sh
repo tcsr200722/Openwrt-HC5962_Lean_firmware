8f649a12c4bd75a99f239d1aeb022a2ec73a2157
chenxudong2020 <61214170+chenxudong2020@users.noreply.github.com>
2020-05-18 21:23:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,11 +7,11 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
-git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
-git clone https://github.com/rosywrt/luci-theme-rosy package/luci-theme-rosy
+#git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+#git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+#git clone https://github.com/rosywrt/luci-theme-rosy package/luci-theme-rosy
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
-git clone https://github.com/openwrt-dev/po2lmo.git
+#git clone https://github.com/openwrt-dev/po2lmo.git
 
 device_name='HC5962'
 
@@ -119,7 +119,7 @@ echo '添加qBittorrent'
 
 
 
-pushd po2lmo
+#pushd po2lmo
 make && sudo make install
 popd
 ./scripts/feeds update -a
